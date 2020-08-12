cf3301a288036d26098591f503f6be122c641676
Dustin <dmkelly100@gmail.com>
2020-08-12 15:43:19 -0400

    added total price

--- src/components/Burger/BurgerControls/BurgerControls.js
+++ src/components/Burger/BurgerControls/BurgerControls.js
@@ -12,14 +12,17 @@ const controls = [
 const burgerControls = (props) => {
 	return (
 		<div className={classes.BurgerControls}>
+			<p>
+				Current Price: <strong>${props.price.toFixed(2)}</strong>
+			</p>
 			{controls.map((ctrl) => {
 				return (
 					<BurgerControl
 						key={ctrl.label}
-                        label={ctrl.label}
-                        added={() => props.ingredientAdded(ctrl.type)}
-                        removed = {()=> props.ingredientRemoved(ctrl.type)}
-                        disabled = {props.disabled[ctrl.type]}
+						label={ctrl.label}
+						added={() => props.ingredientAdded(ctrl.type)}
+						removed={() => props.ingredientRemoved(ctrl.type)}
+						disabled={props.disabled[ctrl.type]}
 					/>
 				);
 			})}
--- src/containers/BurgerMaker/BurgerMaker.js
+++ src/containers/BurgerMaker/BurgerMaker.js
@@ -66,6 +66,7 @@ class BurgerMaker extends Component {
 					ingredientAdded={this.addIngredientHandler}
 					ingredientRemoved={this.removeIngredientHandler}
 					disabled={disabledInfo}
+					price={this.state.totalPrice}
 				/>
 			</Fragment>
 		);
