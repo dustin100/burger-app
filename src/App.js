8cdcda3853a95ca64c41d785fb94644e7c3fc3ac
Dustin <dmkelly100@gmail.com>
2020-08-24 10:23:43 -0400

    added checkout summary components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import Layouts from './components/Layouts/Layouts';
 import BurgerMaker from './containers/BurgerMaker/BurgerMaker';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
 	render() {
@@ -9,6 +10,7 @@ class App extends Component {
 			<div>
 				<Layouts>
 					<BurgerMaker />
+					<Checkout />
 				</Layouts>
 			</div>
 		);
